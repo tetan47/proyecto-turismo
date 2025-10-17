4d66d29780e41700eb7e6af37cde6ef3f303cc13
DeiX-18 <emmaworkbenn@gmail.com>
2025-10-17 14:26:17 -0300

    Agregar la página 'Mis Eventos' y el listado de eventos del backend
    Se añadió mis-eventos.php para mostrar los eventos creados por el organizador conectado, incluyendo la validación de la sesión y la recuperación de eventos. Se introdujo backend/eventos/ListarMisEventos.php para gestionar el listado de eventos filtrados para el organizador. Se actualizaron catalogo.css y estructura_fundamental.css para mejorar la maquetación y el diseño adaptable, y se realizaron pequeños ajustes de formato en Catálogo.php

--- Frontend/Catálogo.php
+++ Frontend/Catálogo.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <link rel="stylesheet" href="css/estructura_fundamental.css">
   <link rel="stylesheet" href="css/catalogo.css">
-  <?php include('../backend/Conexion.php'); ?>
+  <?php include('../backend/Conexion.php'); ?>  
 
 </head>
 
